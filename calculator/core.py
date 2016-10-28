79c9ff352b4866564bc027a18ae987db70fb3bf3
Alexey Kuleshov <alexeykuleshov1990@gmail.com>
2016-10-28 15:36:51 +0200

    All necessary fuctions added (+exceptions)

--- calculator/core.py
+++ calculator/core.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from math import factorial
 
 class Calculator:
 
@@ -7,23 +7,54 @@ class Calculator:
         pass
 
     def sum(self, a, b):
+
+        if not (isinstance(a,int) | isinstance(a,float)):
+            return "Error, first argument is a string value."
+
+        if not (isinstance(b,int) | isinstance(b,float)):
+            return "Error, second argument is a string value."
+
         return a+b
-
-        if not a.isdigit():
-            return "a not number"
-
-        if not b.isdigit():
-            return "b not number"
        
     def multiplication(self, a, b=None):
+
         if b is None:
             return a*a
+
+        if not (isinstance(a,int) | isinstance(a,float)):
+            return "Error, first argument is a string value."
+
+        if not (isinstance(b,int) | isinstance(b,float)):
+            return "Error, second argument is a string value."
 
         return a*b
 
     def poweroftwo(self, value):
-        return 2 ** value
 
-    def division(self, a, b):
+        if not (isinstance(value,int) | isinstance(value,float)):
+            return "Error, the argument is a string value."
+
+        return value ** 2
+
+    def division(self, a,b):
+
+        if (b==0):
+            return 'Error, the second argument can not be 0.'
+
+        if not (isinstance(a,int) | isinstance(a,float)):
+            return "Error, first argument is a string value."
+
+        if not (isinstance(b,int) | isinstance(b,float)):
+            return "Error, second argument is a string value."
+
         return a/b
 
+    def factorial(self, value):
+
+        if not isinstance(value,int):
+            return "Error, argument must be a integer value."
+
+        if not (isinstance(value,int) | isinstance(value,float)):
+            return "Error, first argument is a string value."
+
+        return factorial(value)
